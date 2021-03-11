92b5f49024cefbba367f9d797a9679182fed624f
Alessandro Marega <ale_marega@hotmail.it>
2021-03-11 15:27:58 +0100

    aggiunto l'evento sull'icona 'x' che rimuove l'hamburger-menu

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 // VARIABILI:
 var hamburgerIcon = $(".header-right > a");
 var hamburgerMenu = $(".hamburger-menu");
+var closeIcon = $(".close");
 
 // 1. mostro l'hamburgher-menu al click suyll'icona 'bars'
 hamburgerIcon.click(
   function () {
-    hamburgerMenu.show("fast");
+    hamburgerMenu.addClass("active");
+  }
+);
+
+// 2. nascondo l'hamburgher-menu al click suyll'icona 'x'
+closeIcon.click(
+  function () {
+    hamburgerMenu.removeClass("active");
   }
 );
